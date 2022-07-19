a6a4c6f2ed67d8e6e5415df927ae5dfd023e3744
Písek Pískovec <pisekpiskovec@gmail.com>
2022-07-19 17:44:07 +0200

    hopefuly fixing paths and other places with wrongly spelled project name

--- Titel (WinForm)/Properties/Resources.Designer.cs
+++ Titel (WinForm)/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Titel_WinFrorm.Properties {
+namespace Titel_WinForm.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Titel_WinFrorm.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Titel_WinFrorm.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Titel_WinForm.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
